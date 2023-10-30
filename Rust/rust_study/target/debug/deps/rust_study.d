6e04715a4aaf5e01bf9f3c3910bba637b2e72203
HaruYuumei <luzivotto.erick@gmail.com>
2023-10-30 12:44:50 -0300

    rustCommit

---
+++ Rust/rust_study/target/debug/deps/rust_study.d
@@ -0,0 +1,5 @@
+C:\Users\luziv\Documents\Programming\Rust\rust_study\target\debug\deps\rust_study.exe: src\main.rs
+
+C:\Users\luziv\Documents\Programming\Rust\rust_study\target\debug\deps\rust_study.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/rust_study/target/debug/rust_study.d
@@ -0,0 +1,1 @@
+C:\Users\luziv\Documents\Programming\Rust\rust_study\target\debug\rust_study.exe: C:\Users\luziv\Documents\Programming\Rust\rust_study\src\main.rs
